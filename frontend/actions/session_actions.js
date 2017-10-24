f2467bb8265194c0ec5a3a86915f67e06a8439f9
Corey Ladovsky <coreyskymusic@gmail.com>
2017-10-24 16:47:43 -0400

    added gems

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,3 +1,18 @@
 import * as SessionApiUtil from "../util/session_api_util";
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = (currentUser) =>({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const
+
+export const login = (user) => (dispatch) => {
+  return SessionApiUtil.login(user).then(
+     (u) => dispatch(receiveCurrentUser(u)));
+};
+
+export const
