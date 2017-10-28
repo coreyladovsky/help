056cfcc43821d7239fce0bde93db050e7c46be4b
Corey Ladovsky <coreyskymusic@gmail.com>
2017-10-28 18:47:24 -0400

    halfway through business seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,136 @@
 
 
 Business.create(name: "Hamburger Chilli", address: "701 Flatbush Ave
-Brooklyn, NY 11225", phone_number: "(718) 333-5099", cuisine: "american",
+Brooklyn, NY 11225", phone_number: "(718) 333-5099", cuisine: "American",
 image: File.open("app/assets/images/business_images/american.jpg"),
 mon_start_time: 15, mon_end_time: 24, tue_start_time: 15, tue_end_time: 24, wed_start_time: 15,
 wed_end_time: 24, thur_start_time: 15, thur_end_time: 24, fri_start_time: 15, fri_end_time: 24,
 sat_start_time: 13, sat_end_time:24, sun_start_time: 13, sun_end_time: 24)
 
 Business.create(name: "Pickings & Parry", address: "55 Water St
-Brooklyn, NY 11201", phone_number: "(718) 473-9555", cuisine: "american",
+Brooklyn, NY 11201", phone_number: "(718) 473-9555", cuisine: "American",
 image: File.open("app/assets/images/business_images/american2.jpg"))
+
+Business.create(name: "Mullied", address: "276 Bay Ridge Ave
+Brooklyn, NY 11220", phone_number: "(541) 231-6160", cuisine: "Bar",
+image: File.open("app/assets/images/business_images/bar.jpg"))
+
+Business.create(name: "Old Town Bar", address: "39 W 19th St
+New York, NY 10011", phone_number: "(646) 833-7532", cuisine: "Bar",
+image: File.open("app/assets/images/business_images/bar.jpg"))
+
+Business.create(name: "Central Cafe", address: "95 1st Ave
+New York, NY 10003", phone_number: "(646) 791-5400", cuisine: "Cafe",
+image: File.open("app/assets/images/business_images/cafe.jpg"))
+
+Business.create(name: "Pings", address: "407 Smith St
+Brooklyn, NY 11231", phone_number: "(347) 689-3075", cuisine: "Chinese",
+image: File.open("app/assets/images/business_images/chinese.jpg"))
+
+Business.create(name: "Coughing Coffee", address: "9 Pell St
+New York, NY 10013", phone_number: "(212) 233-8888", cuisine: "Coffee",
+image: File.open("app/assets/images/business_images/coffee.jpg"))
+
+Business.create(name: "The Frenchie", address: "244 Smith St
+Brooklyn, NY 11231", phone_number: "(347) 204-2545", cuisine: "Coffee",
+image: File.open("app/assets/images/business_images/coffee2.jpg"))
+
+Business.create(name: "Starbucks", address: "509 Amsterdam Ave
+New York, NY 10024", phone_number: "(212) 470-5566", cuisine: "Coffee",
+image: File.open("app/assets/images/business_images/coffee3.jpg"))
+
+Business.create(name: "Irish Coffee", address: "22 W 8th St
+New York, NY 10011", phone_number: "(212) 533-2962", cuisine: "Coffee",
+image: File.open("app/assets/images/business_images/coffee4.jpg"))
+
+Business.create(name: "Sea Dog", address: "1 Doyers St
+New York, NY 10013", phone_number: "(212) 791-1817", cuisine: "Coffee",
+image: File.open("app/assets/images/business_images/coffee5.jpg"))
+
+Business.create(name: "The Deli", address: "100 W Houston St
+New York, NY 10012", phone_number: "(212) 254-7000", cuisine: "Delicatessen",
+image: File.open("app/assets/images/business_images/delicatessen.jpg"))
+
+Business.create(name: "Carnigie", address: "488 Carroll St
+Brooklyn, NY 11215", phone_number: "(718) 855-1941", cuisine: "Delicatessen",
+image: File.open("app/assets/images/business_images/delicatessen2.jpg"))
+
+Business.create(name: "Circos", address: "509 Myrtle Ave
+Brooklyn, NY 11205", phone_number: "(718) 783-1936", cuisine: "Dessert",
+image: File.open("app/assets/images/business_images/dessert.jpg"))
+
+Business.create(name: "Cake", address: "424 E 9th St
+New York, NY 10009", phone_number: "(646) 755-8088", cuisine: "Dessert",
+image: File.open("app/assets/images/business_images/dessert2.jpg"))
+
+Business.create(name: "Truffles", address: "5 E 19th St
+New York, NY 10003", phone_number: "(212) 802-9999", cuisine: "Dessert",
+image: File.open("app/assets/images/business_images/dessert3.jpg"))
+
+Business.create(name: "Queens Diner", address: "200 E 3rd St
+New York, NY 10009", phone_number: "(646) 682-7080", cuisine: "Diner",
+image: File.open("app/assets/images/business_images/Diner.jpg"))
+
+Business.create(name: "Grocer", address: "82 Reade St
+New York, NY 10007", phone_number: "(212) 267-3000", cuisine: "Diner",
+image: File.open("app/assets/images/business_images/diner2.jpg"))
+
+usiness.create(name: "All Night", address: "37-11 30th Ave
+Astoria, NY 11103", phone_number: "(718) 606-1900", cuisine: "Diner",
+image: File.open("app/assets/images/business_images/diner3.jpg"))
+
+Business.create(name: "Upside Down", address: "65 4th Ave
+New York, NY 10003", phone_number: "(212) 388-0088", cuisine: "Diner",
+image: File.open("app/assets/images/business_images/diner4.jpg"))
+
+Business.create(name: "Kitchen", address: "199 Grand St
+New York, NY 10013", phone_number: "(212) 334-3669", cuisine: "Diner",
+image: File.open("app/assets/images/business_images/diner6.jpg"))
+
+Business.create(name: "Rock", address: "1046 Flatbush Ave
+Brooklyn, NY 11226", phone_number: "(347) 350-6152", cuisine: "Fast Food",
+image: File.open("app/assets/images/business_images/fastfood.jpg"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
