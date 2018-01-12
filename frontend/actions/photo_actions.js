cd68712744473ac3145e50773f7b4f5d4f094be2
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-12 10:30:38 -0500

    updated photo actions

--- frontend/actions/photo_actions.js
+++ frontend/actions/photo_actions.js
@@ -2,6 +2,7 @@ import * as PhotoAPIUtil from '../util/photo_api_util';
 
 export const RECEIVE_PHOTOS = "RECEIVE_PHOTOS";
 export const RECEIVE_PHOTO = "RECEIVE_PHOTO";
+export const REMOVE_PHOTO = "REMOVE_PHOTO";
 
 export const receivePhotos = photos => ({
   type: RECEIVE_PHOTOS,
@@ -13,10 +14,37 @@ export const receivePhoto = photo => ({
   photo
 });
 
+export const removePhoto = photoId => ({
+  type: REMOVE_PHOTO,
+  photoId
+});
+
 export const fetchBizPhotos = (businessId) => dispatch => (
-  PhotoAPIUtil.fetchBizPhotos(businessId).then(photos => dispatch(receivePhotos(photos)))
+  PhotoAPIUtil.fetchBizPhotos(businessId).then(
+    photos => dispatch(receivePhotos(photos))
+  )
 );
 
 export const fetchPhoto = (photoId) => dispatch => (
-  PhotoAPIUtil.fetchPhoto(photoId).then(photo => dispatch(receivePhoto(photo)))
+  PhotoAPIUtil.fetchPhoto(photoId).then(
+    photo => dispatch(receivePhoto(photo))
+  )
+);
+
+export const createPhoto = data => dispatch => (
+  PhotoAPIUtil.createPhoto(data).then(
+    photo => dispatch(receivePhoto(photo))
+  )
+);
+
+export const deletePhoto = photoId => dispatch => (
+  PhotoAPIUtil.deletePhoto(photoId).then(
+    photo => dispatch(removePhoto(photo.id))
+  )
+);
+
+export const updatePhoto = updatedPhoto => dispatch => (
+  PhotoAPIUtil.updatePhoto(updatedPhoto).then(
+      photo => dispatch(receivePhoto(photo))
+  )
 );
