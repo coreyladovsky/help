65344d3c43a48f74ed4c4686d76f654513d7d3ab
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-12 16:16:16 -0500

    no squished business photos

--- frontend/components/photos/photo_upload.jsx
+++ frontend/components/photos/photo_upload.jsx
@@ -5,6 +5,28 @@ import  { Link } from 'react-router-dom';
 class PhotoUpload extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imageFile: null,
+      imageUrl: null,
+    };
+    this.updateFile = this.updateFile.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateFile(e) {
+    let file = e.currentTarget.files[0];
+    let fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      this.setState({ imageFile: file, imageUrl: fileReader.result });
+    };
+    if(file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  handleSubmit(e) {
+    let formData = new FormData();
+    formData.append("bussiness[image]", this.state.imageFile);
   }
 
   render() {
@@ -15,6 +37,12 @@ class PhotoUpload extends React.Component {
         intendedPage={this.props.intendedPage}
         logout={this.props.logout} nextPage={this.props.nextPage}
         clearPage={this.props.clearPage}/>
+
+      <div className="image-upload">
+        <input type="file" onChange={this.updateFile}/>
+        <img src={this.state.imageUrl}/>
+      </div>
+
     </div>
   );
   }
