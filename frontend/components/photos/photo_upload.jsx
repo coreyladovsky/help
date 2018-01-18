23e3dc2885335bb5d55c9efb3e9cca7694a9a2e5
Corey Ladovsky <coreyskymusic@gmail.com>
2018-01-18 12:02:55 -0500

    spinner working on upload

--- frontend/components/photos/photo_upload.jsx
+++ frontend/components/photos/photo_upload.jsx
@@ -8,11 +8,13 @@ class PhotoUpload extends React.Component {
     this.state = {
       imageFile: null,
       imageUrl: null,
+      loading: false
     };
     this.updateFile = this.updateFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.photoText = this.photoText.bind(this);
     this.submitPhoto = this.submitPhoto.bind(this);
+    this.spinSpinner = this.spinSpinner.bind(this);
   }
 
   componentDidMount() {
@@ -46,6 +48,18 @@ class PhotoUpload extends React.Component {
     }
   }
 
+  spinSpinner() {
+    if(this.state.loading) {
+      return(
+        <div className="spin-container">
+          <div className="small-spin">
+            <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+          </div>
+        </div>
+        );
+    }
+  }
+
   updateFile(e) {
     let file = e.currentTarget.files[0];
     let fileReader = new FileReader();
@@ -72,8 +86,12 @@ class PhotoUpload extends React.Component {
     formData.append("photo[image]", this.state.imageFile);
     formData.append("photo[user_id]", this.props.currentUser.id);
     formData.append("photo[business_id]", this.props.business.id);
-    this.props.createPhoto(formData).then(() => {
+    this.setState({ loading: true });
+    this.props.createPhoto(formData).then((res) => {
+      this.setState({ loading: false });
       this.props.history.push(`/businesses/${this.props.business.id}`);
+    }, (res) => {
+      this.setState({ loading: false });
     });
 
   }
@@ -109,12 +127,12 @@ class PhotoUpload extends React.Component {
         <form>
           <div className="image-upload">
             {this.photoText()}
+            {this.spinSpinner()}
             <input className="image-uploaded" type="file" onChange={this.updateFile}/>
             <img className="image-preview" src={this.state.imageUrl}/>
           </div>
           {this.submitPhoto()}
         </form>
-        <div>Cancel</div>
 
       </div>
 
