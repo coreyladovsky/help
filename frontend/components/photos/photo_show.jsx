43842bd9f131124a92dc4437f4a4916bf5629a5e
Corey Ladovsky <coreyskymusic@gmail.com>
2018-02-02 16:19:31 -0500

    picture scrolling dependent on click position on photo

--- frontend/components/photos/photo_show.jsx
+++ frontend/components/photos/photo_show.jsx
@@ -7,6 +7,7 @@ class PhotoShow extends React.Component {
     this.pageReturn = this.pageReturn.bind(this);
     this.nextPhoto = this.nextPhoto.bind(this);
     this.lastPhoto = this.lastPhoto.bind(this);
+    this.photoNext = this.photoNext.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +20,14 @@ class PhotoShow extends React.Component {
     document.querySelector("html").setAttribute("style", "overflow: auto;");
   }
 
+  photoNext(e) {
+    if(e.nativeEvent.offsetX < 150) {
+      this.lastPhoto();
+    } else {
+      this.nextPhoto();
+    }
+  }
+
   nextPhoto() {
     let next = (this.props.photoNumber + 1) % this.props.photos.length;
     this.props.history.push(
@@ -27,7 +36,6 @@ class PhotoShow extends React.Component {
   }
 
   lastPhoto() {
-
     let last = this.props.photoNumber - 1;
     if(last < 0) {
       last = last + this.props.photos.length;
@@ -52,10 +60,14 @@ class PhotoShow extends React.Component {
 
       return (
         <div className="photo-modal" value="exit" onClick={this.pageReturn}>
+          <div className="close-container">
+            <div className="close-modal" value="exit">Close <i className="fal fa fa-times"></i></div>
+          </div>
           <div className="photo-content">
             <div className="left-scroll" onClick={this.lastPhoto}><i className="far fa fa-angle-left angles"></i></div>
             <div className="photo-of-photo-show" >
-              <img className="photo-image-show" src={this.props.photo.image_medium} />
+              <img className="photo-image-show" src={this.props.photo.image_medium} onClick={this.photoNext}/>
+              <div className="trans-photo-bar"></div>
             </div>
             <div className="right-scroll" onClick={this.nextPhoto}><i className="far fa fa-angle-right angles"></i></div>
           </div>
